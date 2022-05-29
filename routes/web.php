c3ebc58e6c283f320d9123a2209e7afb31ec572a
brianadriansyah <brianadri45@gmail.com>
2022-05-29 16:53:34 +0700

    oke

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::get('/peminjaman', 'PeminjamanController@bukutampil');
 
 // Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
 Route::post('/buku/tambah','BukuController@bukutambah');
-<<<<<<< HEAD
 Route::post('/petugas/tambah','PetugasController@petugastambah');
+Route::post('/siswa/tambah','SiswaController@siswatambah');
 
 // Selanjutnya kita buatkan route baru pada routes/web.php
 Route::get('/buku/hapus/{idbuku}','BukuController@bukuhapus');
@@ -45,7 +45,6 @@ Route::get('/petugas/hapus/{idpetugas}','PetugasController@petugashapus');
 // Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
 Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
 Route::put('/petugas/edit/{idpetugas}', 'PetugasController@petugasedit');
-=======
 Route::post('/siswa/tambah','SiswaController@siswatambah');
 
 // Selanjutnya kita buatkan route baru pada routes/web.php
@@ -55,7 +54,6 @@ Route::post('/siswa/hapus/{idsiswa}','SiswaController@siswatambah');
 // Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
 Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
 Route::post('/siswa/edit/{idsiswa}','SiswaController@siswatambah');
->>>>>>> 2ff53ec3fd67e6d953f9b0a222f24e9616bfc340
 
 // Pada sintak tersebut terdapat link yang mengarahkan ke alamat /pinjam, maka selanjutnya kita buat route baru pada file routes/web.php dengan sintak seperti berikut
 Route::get('/pinjam', 'PinjamController@pinjamtampil');
