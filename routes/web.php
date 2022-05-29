c8f26ecbf7ec212289f6336d27120ef5af2fe7f9
sungshot99 <fakhri.r.13@gmail.com>
2022-05-29 17:40:13 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -17,72 +17,83 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-// Route / dan route home
+// ROUTE HOME
 Route::get('/', function () {
     return view('halaman/view_home');
 });
 
 // // Route Home
 // Route::get('/home', function(){
-//     return view('home');
-// });
-
-//Route untuk Data Buku
+    //     return view('home');
+    // });
+    
+//ROUTE BUKU
 Route::get('/buku', 'BukuController@bukutampil');
+Route::post('/buku/tambah','BukuController@bukutambah');    
+Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
+Route::get('/buku/hapus/{idbuku}','BukuController@bukuhapus');
 
-//Route untuk Data Siswa
+//ROUTE SISWA
 Route::get('/siswa', 'SiswaController@siswatampil');
-
-//Route untuk Data Petugas
-Route::get('/petugas', 'PetugasController@petugastampil');
-
-// Route Petugas Tambah
-Route::post('/petugas/tambah', 'PetugasController@petugastambah');
-
-// Route Petugas Edit
-Route::put('/petugas/edit/{idpetugas}', 'PetugasController@petugasedit');
-
-// Route Petugas Hapus DELETE
-Route::get('/petugas/hapus/{idpetugas}', 'PetugasController@petugashapus');
-
-//Route untuk Data Peminjaman
-Route::get('/peminjaman', 'PeminjamanController@bukutampil');
-
-// Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
-Route::post('/buku/tambah','BukuController@bukutambah');
-Route::post('/petugas/tambah','PetugasController@petugastambah');
 Route::post('/siswa/tambah','SiswaController@siswatambah');
-
-// SISWA TAMBAH
-Route::post('/siswa/tambah','SiswaController@siswatambah');
-
-// SISWA HAPUS
+Route::put('/siswa/edit/{idsiswa}','SiswaController@siswaedit');
 Route::get('/siswa/hapus/{idsiswa}','SiswaController@siswahapus');
 
-// Selanjutnya kita buatkan route baru pada routes/web.php
-Route::get('/buku/hapus/{idbuku}','BukuController@bukuhapus');
-Route::get('/petugas/hapus/{idpetugas}','PetugasController@petugashapus');
-
-// Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
-Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
+//ROUTE PETUGAS
+Route::get('/petugas', 'PetugasController@petugastampil');
+Route::post('/petugas/tambah', 'PetugasController@petugastambah');
 Route::put('/petugas/edit/{idpetugas}', 'PetugasController@petugasedit');
-Route::post('/siswa/tambah','SiswaController@siswatambah');
+Route::get('/petugas/hapus/{idpetugas}', 'PetugasController@petugashapus');
 
-// Selanjutnya kita buatkan route baru pada routes/web.php
-Route::get('/buku/hapus/{idbuku}','BukuController@bukuhapus');
-Route::post('/siswa/hapus/{idsiswa}','SiswaController@siswatambah');
+// ROUTE PEMINJAMAN
+Route::get('/pinjam', 'PinjamController@pinjamtampil');
+Route::post('/pinjam/tambah','PinjamController@pinjamtambah');
+Route::put('/pinjam/edit/{idpinjam}', 'PinjamController@pinjamedit');
+Route::get('/pinjam/hapus/{idpinjam}','PinjamController@pinjamhapus');
+// Route::get('/peminjaman', 'PeminjamanController@bukutampil');
+
+
+
+// Route Petugas Tambah
+
+// Route Petugas Edit
+
+// Route Petugas Hapus DELETE
+
+//Route untuk Data Peminjaman
 
 // Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
-Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
-Route::put('/siswa/edit/{idsiswa}','SiswaController@siswatambah');
+// Route::post('/petugas/tambah','PetugasController@petugastambah');
+
+
+// SISWA TAMBAH
+// Route::post('/siswa/tambah','SiswaController@siswatambah');
+
+// SISWA HAPUS
+// Route::get('/siswa/hapus/{idsiswa}','SiswaController@siswahapus');
+
+// Selanjutnya kita buatkan route baru pada routes/web.php
+// Route::get('/petugas/hapus/{idpetugas}','PetugasController@petugashapus');
+
+// Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
+// Route::put('/buku/edit/{idbuku}', 'BukuController@bukuedit');
+// Route::put('/petugas/edit/{idpetugas}', 'PetugasController@petugasedit');
+// Route::post('/siswa/tambah','SiswaController@siswatambah');
+
+// Selanjutnya kita buatkan route baru pada routes/web.php
+// Route::get('/buku/hapus/{idbuku}','BukuController@bukuhapus');
+
+
+// Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
+
 
 // Pada sintak tersebut terdapat link yang mengarahkan ke alamat /pinjam, maka selanjutnya kita buat route baru pada file routes/web.php dengan sintak seperti berikut
-Route::get('/pinjam', 'PinjamController@pinjamtampil');
+
 
 // Pada modal tambah terdapat sintak :
 // <form name="formpinjamtambah" id="formpinjamtambah" action="/pinjam/tambah" method="post" enctype="multipart/form-data">
 // form tersebut mengarahkan action ke alamat /pinjam/tambah, maka selanjutnya kita buat route baru pada routes/web.php dengan sintak seperti berikut ini :
-Route::post('/pinjam/tambah','PinjamController@pinjamtambah');
+// Route::post('/pinjam/tambah','PinjamController@pinjamtambah');
 
 // Pada view_pinjam.blade.php terdapat button Delete
 // <a href="pinjam/hapus/{{$p->idpinjam}}" onclick="return confirm('Yakin mau dihapus?')">
@@ -91,9 +102,9 @@ Route::post('/pinjam/tambah','PinjamController@pinjamtambah');
 // </button>
 // </a>
 // Selanjutnya kita buatkan route baru pada routes/web.php
-Route::get('/pinjam/hapus/{idpinjam}','PinjamController@pinjamhapus');
+// Route::get('/pinjam/hapus/{idpinjam}','PinjamController@pinjamhapus');
 
 // Dalam modalPinjamEdit tersebut terdapat form dengan action yang diarahkan ke alamat /pinjam/edit/{{ $p->idpinjam}},
 // <form name="formpinjamedit" id="formpinjamedit" action="/pinjam/edit/{{ $p->idpinjam}} " method="post" enctype="multipart/form-data">
 // Oleh karena itu selanjutnya kita buatkan route baru pada routes/web.php
-Route::put('/pinjam/edit/{idpinjam}', 'PinjamController@pinjamedit');
+// Route::put('/pinjam/edit/{idpinjam}', 'PinjamController@pinjamedit');
