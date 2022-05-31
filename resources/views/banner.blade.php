a3b69cc01ca0479a4528c9a5528834bf552afac7
sungshot99 <fakhri.r.13@gmail.com>
2022-05-31 19:50:50 +0700

    update banner and sibedar

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,5 +1,5 @@
 <!-- Awal BANNER -->
 <div class="col-md-12">
-    <img src={{url('/assets/gambar/banner.jpg')}} width="100%" height="225px" alt="banner" style="box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px; border-radius: 15px">
+    <img src={{url('/assets/gambar/banner-fix.jpg')}} width="100%" height="225px" alt="banner" style="box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px; border-radius: 15px">
 </div>
 <!-- akhir BANNER -->
